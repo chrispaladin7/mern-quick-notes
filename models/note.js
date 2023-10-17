ad246d925b447c375836899780e0309b97e212a9
chrispaladin7 <chrismartey@yahoo.com>
2023-10-17 15:33:24 -0400

    Adds Note model

---
+++ models/note.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const noteSchema = new Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }, 
+    timestamps: true
+})
+
+
+module.exports = mongoose.model('Note', noteSchema);
